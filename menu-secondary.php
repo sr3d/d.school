0bbff63f0f5e5e25a9103d21cacda7bc72b6e878
Jason Conroy <jason@findingsimple.com>
2012-01-25 15:33:36 +1100

    Limit banner menu depth to 1 level

--- menu-secondary.php
+++ menu-secondary.php
@@ -12,7 +12,7 @@ if ( has_nav_menu( 'secondary' ) ) : ?>
 
 	<?php do_atomic( 'before_menu_secondary' ); // dschool_before_menu_secondary ?>
 
-	<?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container' => false ,'container_class' => '', 'menu_class' => '', 'menu_id' => 'banner-links', 'fallback_cb' => '', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container' => false ,'container_class' => '', 'menu_class' => '', 'menu_id' => 'banner-links', 'fallback_cb' => '', 'link_before' => '<span>', 'link_after' => '</span>', 'depth' => 1 ) ); ?>
 
 	<?php do_atomic( 'after_menu_secondary' ); // dschool_after_menu_secondary ?>
 
